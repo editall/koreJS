a914351121f51e8579a8993807fa61ada89e1bb0
hika <hika00@gmail.com>
2024-03-13 15:28:23 +0900

    .

--- js/app.js
+++ js/app.js
@@ -9,8 +9,7 @@ try {
 Kore.isMainThread = !Kore.isWorkerThread;
 Kore.decoder = new TextDecoder();
 //console.log(Array.from(new Uint8Array(view.buffer)).reduce((a, c)=>a+','+c, ''));
-Kore.parseBuffer = (buffer, v = {c:0})=>{
-  const view = new DataView(buffer);
+Kore.parseBuffer = (view, v = {c:0})=>{
   let cursor = v.c;
   const type = view.getInt8(cursor);
   switch (type) {
@@ -27,7 +26,7 @@ Kore.parseBuffer = (buffer, v = {c:0})=>{
     case 2:{ // string
       const length = view.getInt32(cursor + 1);
       const buf = new Uint8Array(new ArrayBuffer(length));
-      buf.set(new Uint8Array(buffer.slice(cursor + 5, cursor + 5 + length)));
+      buf.set(new Uint8Array(view.buffer.slice(cursor + 5, cursor + 5 + length)));
       v.v = Kore.decoder.decode(buf);
       v.c = cursor + 5 + length;
       return v;
@@ -42,7 +41,7 @@ Kore.parseBuffer = (buffer, v = {c:0})=>{
       const array = [];
       let i = 0;
       while (i < length) {
-        const {v:value, c:nextCursor} = Kore.parseBuffer(buffer, {c:cursor + 5});
+        const {v:value, c:nextCursor} = Kore.parseBuffer(view, {c:cursor + 5});
         array.push(value);
         cursor = nextCursor;
         i++;
@@ -56,8 +55,8 @@ Kore.parseBuffer = (buffer, v = {c:0})=>{
       const object = Object.create(null);
       let i = 0;
       while (i < length) {
-        const {v:key, c:nextCursor} = Kore.parseBuffer(buffer, {c:cursor + 5});
-        const {v:value, c:nextNextCursor} = Kore.parseBuffer(buffer, {c:nextCursor});
+        const {v:key, c:nextCursor} = Kore.parseBuffer(view, {c:cursor + 5});
+        const {v:value, c:nextNextCursor} = Kore.parseBuffer(view, {c:nextCursor});
         object[key] = value;
         cursor = nextNextCursor;
         i++;
@@ -74,7 +73,7 @@ if(Kore.isWorkerThread){
     if(crossOriginIsolated && event.data instanceof SharedArrayBuffer){
       const view = new DataView(event.data);
       const id = view.getUint8(0);
-      const data = Kore.parseBuffer(event.data, {c:2}).v;
+      const data = Kore.parseBuffer(view, {c:2}).v;
       console.log('worker sab', id, data);
       setTimeout(_=>{
         view.setUint8(1, 1); // 0: not completed, 1: completed
@@ -90,6 +89,124 @@ if(Kore.isWorkerThread){
     }
   }
 }else{
+  Kore.binder = limit=> Kore.Binder.add(limit);
+  Kore.Binder = class{
+    static items = [];
+    static f = _=>{
+      let i = 0, j = this.items.length;
+      while(i < j){
+        this.items[i].process();
+        i++;
+      }
+      requestAnimationFrame(this.f);
+    }
+    static #isInit = false;
+    static init(){
+      if(this.#isInit) return;
+      this.#isInit = true;
+      requestAnimationFrame(this.f);
+    }
+    static add(limit){
+      const binder = new this(limit);
+      this.items.push(binder);
+      this.init();
+      return binder;
+    }
+    #queue = Object.create(null)
+    #limit;
+    constructor(limit){
+      this.#limit = limit;
+
+    }
+    add(data){
+      const id = data.view.viewId;
+      (this.#queue[id] || (this.#queue[id] = Object.create(null)))[data.name] = data;
+    }
+    process(){
+      const start = performance.now();
+      for(const id in this.#queue){
+        const items = this.#queue[id];
+        for(const key in items){
+          const data = items[key];
+          const el = data.view.el;
+          switch(data.type){
+            case"f":
+              View.setF[data.name](data.view, data.value);
+              break;
+            case "attr":
+              el.setAttribute(data.name, data.value);
+              break;
+            case "prop":
+              el[data.name] = data.value;
+              break;
+            case "style":
+              el.style[data.name] = data.value;
+              break;
+          }
+          delete items[key];
+          if(performance.now() - start > this.#limit) return;
+        }
+        delete this.#queue[id];
+      }
+    }
+  }
+  Kore.view = (binder, el)=> new Kore.View(binder, el);
+  Kore.View = class{
+    static viewId = 0;
+    static setF = Object.create(null);
+    static getF = Object.create(null);
+    static get(name){
+      return this.getF[name];
+    }
+    #children;
+    constructor(binder, el){
+      this.binder = binder;
+      this.el = el;
+      el.view = this;
+      this.#children = {};
+      this.id = this.constructor.viewId++;
+    }
+    set(name, value){
+      if(this.constructor.setF[name]) throw new Error(`no setter: ${name}`);
+      this.binder.add({type:"f", view:this, name, value});
+    }
+    get(name){
+      return this.constructor.get(name);
+    }
+    setAttr(name, value){
+      this.binder.add({type:"attr", view:this, name, value});
+    }
+    getAttr(name){
+      return this.el.getAttribute(name);
+    }
+    setProp(name, value){
+      this.binder.add({type:"prop", view:this, name, value});
+    }
+    getProp(name){
+      return this.el[name];
+    }
+    setStyle(name, value){
+      this.binder.add({type:"style", view:this, name, value});
+    }
+    getStyle(name){
+      return this.el.style[name];
+    }
+    setContent(content, block){
+      this.el.innerHTML = content;
+      this.#children = {};
+      this.el.querySelectorAll("[kore]").forEach(el=>{
+        this.#children[el.getAttribute("kore")] = new this.constructor(this.binder, el);
+        this.render(block);
+      });
+    }
+    sub(name){
+      return this.#children[name];
+    }
+    render(block){
+      block(this);
+    }
+  }
+
   Kore.Thread = class{
     static READY = Object.create(null);
     static WORKING = Object.create(null);
@@ -140,7 +257,7 @@ if(Kore.isWorkerThread){
       this.#listener.push(callback);
     }
     static postMessage(data){
-      Kore.Thread.init();
+      this.init();
       let id = 0, j = this.#pool.length;
       while(id < j){
         if(this.#states[id] === this.READY){
@@ -155,7 +272,7 @@ if(Kore.isWorkerThread){
     static #view = new DataView(new ArrayBuffer(1));
     static postBuffer(data){
       if(!crossOriginIsolated) throw new Error('SharedArrayBuffer is not allowed');
-      Kore.Thread.init();
+      this.init();
       let id = 0, j = this.#pool.length;
       while(id < j){
         if(this.#states[id] === this.READY){
@@ -189,7 +306,7 @@ if(Kore.isWorkerThread){
         case 'string':
           const bytes = this.#encoder.encode(v);
           const length = bytes.length;
-          if(cursor + 5 + v.length > Kore.bufferSize) throw new Error('buffer overflow');
+          if(cursor + 5 + length > Kore.bufferSize) throw new Error('buffer overflow');
           view.setInt8(cursor, 2); // 2: string
           view.setInt32(cursor+1, length);
           let i = 0;
